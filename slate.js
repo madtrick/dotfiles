4e8ae750426b412b43f2b6007b4e369aaacc8a71
Farruco Sanjurjo <madtrick@gmail.com>
2013-05-07 10:02:31 +0200

    Dump commit

--- slate.js
+++ slate.js
@@ -68,43 +68,148 @@ var fullHeight = S.op("move", {
 });
 
 var widthSlice = function(slice){
+  var width = "trunc(screenSizeX*" + slice + ")";
   return S.op("move",{
       "x" : "windowTopLeftX",
       "y" : "windowTopLeftY",
-      "width" : function(){
-        return Math.round(S.screen().rect().width*slice);
-      },
+      "width" : width,
       "height" : "windowSizeY"
   });
 };
 
+var twoMonitorThrow = function(to){
+  var screenReference;
+
+  if (to === "left") screenReference = "0";
+  if (to === "right") screenReference = "1";
+  if (to === "down") screenReference = "1";
+  if (to === "up") screenReference = "0";
+
+  return S.op("throw",{
+    "screen" : screenReference
+  });
+};
+
 var moveTo = function(to){
+  var x;
+
+  if (to === "right"){
+    x = "screenSizeX-windowSizeX";
+  }
+
+  if (to === "left"){
+    x = "screenOriginX";
+  }
+
   return S.op("move",{
-      "x" : function(){
-        var x,
-            windowWidth = S.app().mainWindow().size().width,
-            screenWidth = S.screen().rect().width;
-
-        if (to === "left"){
-          x = screenWidth - windowWidth;
-        }
-
-        if (to === "right"){
-          x = "screenOriginX";
-        }
-
-        return x;
-      },
+      "x" : x,
       "y" : "windowTopLeftY",
       "width" : "windowSizeX",
       "height" : "windowSizeY"
   });
 };
 
-/*
- * Bindings
- */
+var focusTo = function(to){
+  return S.op("focus", {
+    direction : to
+  });
+};
 
+
+/************************************
+ * Screen setups
+ ************************************/
+var monitorsResolutions = {
+  samsung : "2560x1440",
+  macbook : "1440x900"
+};
+
+var twoMonitorScreenConfigBigUpLaptopDown = {
+  resolutions : [monitorsResolutions.samsung, monitorsResolutions.macbook],
+  screens : {
+    samsung : 0,
+    macbook : 1
+  }
+};
+
+var oneMonitorScreenConfigBig = {
+  resolutions : [monitorsResolutions.samsung],
+  screens : {
+    samsung : 0
+  }
+};
+
+/************************************
+ * Layout setups
+ ************************************/
+var twoMonitorLayouBigUpLaptopDown = S.layout("twoMonitorLayouBigUpLaptopDown", {
+  "iTerm" : {
+    operations : [fullscreen]
+  },
+  "Google Chrome" : {
+    operations : [S.op("move", {
+      x : "screenOriginX",
+      y : "screenOriginY",
+      width : "screenSizeX",
+      height : "screenSizeY",
+      screen : twoMonitorScreenConfigBigUpLaptopDown.screens.macbook
+    })]
+  }
+});
+
+
+var oneMonitorLayoutBig = S.layout("oneMonitorLayoutBig", {
+  "iTerm" : {
+    /*
+     * NOTE: 
+     * I have to use a function to apply to operations to the same window as "chain" and "sequence" didn't work
+     */
+     
+    operations : [function(win){
+      win.doOperation(widthSlice(8/12));
+      win.doOperation(fullHeight);
+      win.doOperation(moveTo("left"));
+    }]
+  },
+  "Google Chrome" : {
+    /*
+     * NOTE: using the win object to move the Chrome window doesn't work 
+     * see https://github.com/jigish/slate/issues/287
+     */
+    //operations : [function(win){
+    //  win.doOperation(widthSlice(4/12));
+    //  win.doOperation(fullHeight);
+    //  win.doOperation(moveTo("right"));
+    //}]
+    operations : [S.op("move",{
+      x : "screenSizeX-(trunc(screenSizeX*4/12))",
+      y : "screenOriginY",
+      width : "trunc(screenSizeX*4/12)",
+      height: "screenSizeY"
+    })]
+  }
+});
+
+var latexEditionLayout = S.layout("latexEditionLayout", {
+  "iTerm" : {
+    operations : [function(win){
+      win.doOperation(widthSlice(8/12));
+      win.doOperation(fullHeight);
+      win.doOperation(moveTo("left"));
+    }]
+  },
+  "Preview" : {
+  operations : [function(win){
+      win.doOperation(widthSlice(4/12));
+      win.doOperation(fullHeight);
+      win.doOperation(moveTo("right"));
+    }]
+  }
+});
+
+/************************************
+ * Binding setups
+ ************************************/
 S.bind("g:alt;cmd", fullscreen);
 S.bind("c:alt;cmd", center);
 S.bind("1:alt;cmd", quarterTopRight);
@@ -115,9 +220,26 @@ S.bind("h:alt;cmd", halfScreen);
 S.bind("l:alt;cmd", fullHeight);
 S.bind("4:shift;cmd", widthSlice(4/12));
 S.bind("8:shift;cmd", widthSlice(8/12));
+S.bind("left:cmd;shift", twoMonitorThrow("left"));
+S.bind("right:cmd;shift", twoMonitorThrow("right"));
+S.bind("up:cmd;shift", twoMonitorThrow("up"));
+S.bind("down:cmd;shift", twoMonitorThrow("down"));
+S.bind("left:cmd", focusTo("left"));
+S.bind("right:cmd", focusTo("right"));
+S.bind("up:cmd", focusTo("up"));
+S.bind("down:cmd", focusTo("down"));
 
 S.bind("l:ctrl;alt;cmd", moveTo("left"));
 S.bind("k:ctrl;alt;cmd", moveTo("right"));
+
+S.bind("l:fn;ctrl;alt", S.op("layout", {name : latexEditionLayout}));
+S.bind("p:fn;ctrl;alt", S.op("layout", {name : oneMonitorLayoutBig}));
+
+/************************************
+ * Default setups
+ ************************************/
+S.default(twoMonitorScreenConfigBigUpLaptopDown.resolutions, twoMonitorLayouBigUpLaptopDown);
+S.default(oneMonitorScreenConfigBig.resolutions, oneMonitorLayoutBig);
 /*
  * Helpers
  */
